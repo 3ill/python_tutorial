95a0667eb78241259f2ae56a2971d009cc00f097
3illBaby <thrillanon@outlook.com>
2025-12-27 18:27:28 +0100

    chore(string_manipulation.py): renaned app.py
    A file to learn the basics of string manipulation in python

---
+++ string_mainpulation.py
@@ -0,0 +1,39 @@
+
+# String Manipulation
+# This returns the length of the string
+newString = "I am learning python, i am liking it"
+stringLength = len(newString)
+
+
+
+# Slice
+# This returns the first 3 characters
+print(newString[0:3])
+
+
+# Escape sequence
+course = "Python \nProgramming"
+print(course)
+
+
+# Concatenation
+# This will join the result of two strings
+first = "Chike"
+last = "Egbu"
+
+full = first + " " + last
+print(full)
+
+
+# Formatted Strings
+formatted_full = f"{first} {last}"
+print(formatted_full)
+
+# Find
+print(first.find("C", 0))
+
+random_string = " Chike Egbu "
+print(random_string.strip())
+
+
+print("Chike" in random_string)
