c348298781198139b40e4cde59096a64f9d8828d
3illBaby <thrillanon@outlook.com>
2025-12-27 17:41:14 +0100

    chore(pyproject.toml): extended commit tool

---
+++ app.py
@@ -0,0 +1,11 @@
+# Boolean
+isPublished = True
+
+
+print(isPublished)
+
+# String Manipulation
+newString = "I am learning python, i am liking it"
+stringLength = len(newString)
+
+print(stringLength)
