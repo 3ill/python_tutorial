0c542c9f0eb8c940878792f7300c2fe365662429
3illBaby <thrillanon@outlook.com>
2025-12-30 16:10:49 +0100

    chore(loops.py): added examples on performing loops in python
    clean implementation with the range there

--- lessons/loops.py
+++ lessons/loops.py
@@ -1,3 +1,8 @@
 # range is a special python function that states the amount of times code in the for loop should run for
-for number in range(5):
-    print(f"attempt {number}")
+for number in range(1):
+    print(f"attempt {number},", (number) * ".")
+
+
+# You can also pass a start and end value, the last value is non inclusive, the code will only run 5 times in this case
+for number in range(1, 6):
+    print(f"attempt {number},", (number) * ".")
