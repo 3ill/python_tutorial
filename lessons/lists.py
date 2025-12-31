a2cc2481db836c70114ffb38cc25a5474a2934a8
3illBaby <thrillanon@outlook.com>
2025-12-31 22:04:56 +0100

    chore(lists.py): added lessons on list methods

--- lessons/lists.py
+++ lessons/lists.py
@@ -25,3 +25,51 @@ matrix[0][2] = 5
 for row in matrix:
     for item in row:
         print(item)
+
+
+# List Methods
+new_numbers = [5, 2, 10, 1, 17, 10]
+# append adds to the end of the list
+new_numbers.append(21)
+
+# insert accepts index and value as param
+new_numbers.insert(1, 3)
+
+# remove will remove the item passed as argument from the list
+# It will throw an error if the element does not exist in the list
+new_numbers.remove(21)
+print(new_numbers)
+
+
+# By default, pop will remove and return the last item in the list but it also accepts an index as args.
+# If index is provided, it will remove and return the element at that index
+# popped_index = new_numbers.pop()
+# print(f"removed {popped_index}")
+
+index = new_numbers.index(17)
+print(f"17 is at index {index}")
+
+# clear will empty the list
+# new_numbers.clear()
+# print(new_numbers)
+
+# This will return a boolean
+# This is the best whenn trying to check for the existence of an item in a list
+print(17 in new_numbers)
+
+# The count method accepts a param of an element in the lists and returns the number of items that element appears in the list
+print(new_numbers.count(10))
+
+# The sort method sorts a list in ascending order, the reverse sorts a list in descending order
+new_numbers.sort()
+new_numbers.reverse()
+print(f"sorted lists {new_numbers}")
+
+# The copy returns a copy of the list as a new list
+# The new lists is independent of the original list, changes made to the original list does not affect the copy
+new_numbers2 = new_numbers.copy()
+new_numbers.append(30)
+
+
+print(new_numbers)
+print(f"copy: {new_numbers2}")
