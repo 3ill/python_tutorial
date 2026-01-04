f41f29ad8ec7a82cc791aff555279ef12d5e2e72
3illBaby <thrillanon@outlook.com>
2026-01-04 23:22:06 +0100

    chore(inheritance.py): extended lesson

--- lessons/inheritance.py
+++ lessons/inheritance.py
@@ -1,7 +1,7 @@
 class MakeRequest:
     # The `__init__` method is a special method (the constructor) that gets called when you create a new `MakeRequest` object
-    def __init__(self) -> None:
-        self.url = "https://example.com"
+    def __init__(self, _url="https://example.com") -> None:
+        self.url = _url
 
     def get_url(self, _endpoint: str):
         return f"{self.url}/{_endpoint}"
@@ -11,7 +11,7 @@ class RequestHandler(MakeRequest):
     pass
 
 
-handler = RequestHandler()
+handler = RequestHandler("https://new-example.com")
 
 url = handler.get_url("login")
 print(url)
